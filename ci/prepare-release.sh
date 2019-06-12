4d8d43f6ef70e9e5657023438e315936d8aaf84b
andriy-ilin <andriy.ilin90@gmail.com>
2019-06-12 11:59:39 +0300

    ci: prepare deploy to now

--- ci/prepare-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Setup git
-git config --global user.email "deploy@travis-ci.org"
-git config --global user.name "Deployment Bot"
-git config --global credential.helper "store"
-echo "https://${GITHUB_TOKEN}:@github.com" > $HOME/.git-credentials
-git checkout $TRAVIS_BRANCH
